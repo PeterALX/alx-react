268e4225b1a62e6546e32a9279f17eb1a3a05a8e
PeterALX <quaypeter25@gmail.com>
2024-10-31 13:04:23 +0300

    done task 1

---
+++ task_1/dashboard/src/Notifications.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Notifications.css";
+
+export default function Notifications() {
+  return <div className="Notifications">Here is the list of notifications</div>;
+}
