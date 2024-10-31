31e1bc8c5d724064c1cb39d62887c08b31fdd62f
PeterALX <quaypeter25@gmail.com>
2024-10-31 13:55:42 +0300

    done task 3 and 4

--- task/dashboard/src/utils.js
+++ task/dashboard/src/utils.js
@@ -5,5 +5,8 @@ function getFooterCopy(isIndex) {
   if (isIndex) return "Holberton School";
   else return "Holberton School main dashboard";
 }
+function getLatestNotification() {
+  return "<strong>Urgent requirement</strong> - complete by EOD";
+}
 
-export { getFullYear, getFooterCopy };
+export { getFullYear, getFooterCopy, getLatestNotification };
