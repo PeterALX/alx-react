623cb6c1f08c44acb71cbcb265210d26cecce397
PeterALX <quaypeter25@gmail.com>
2024-11-07 15:59:06 +0300

    done task 1

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,15 @@
+const _ = require('lodash')
+const $ = require('jquery')
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let clickCount = 0
+function updateCounter() {
+	clickCount++
+	$('#count').text(`${clickCount} clicks on the button`)
+}
+
+$('button').on('click',  _.debounce( updateCounter, 500, { leading: true, trailing: false }))
---
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	entry:'./js/dashboard_main.js',
+	output: {
+		path: __dirname + '/public',
+		filename: 'bundle.js'
+	}
+}
