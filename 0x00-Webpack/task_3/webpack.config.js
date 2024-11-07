e4d363a50dcebd10d484bfc29c4c45fc71381378
PeterALX <quaypeter25@gmail.com>
2024-11-07 18:42:23 +0300

    doing task 3

---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+	mode: 'development',
+	entry:'./js/dashboard_main.js',
+	watch: true,
+	output: {
+		path: __dirname + '/public',
+		filename: 'bundle.js'
+	},
+	devServer: {
+		port: 8564,
+	},
+	module: {
+		rules: [
+			{test: /\.css$/i, use: ['style-loader', 'css-loader']},
+			{
+				test: /\.(gif|png|jpe?g|svg)$/i,
+				use: [
+					/**
+					file-loader causes the image not to display, and outputs two
+					images instead of 1. My guess is that this is because image-webpack-loader
+					is deprecated and therefore broken.image-webpack-loader
+					works fine if file-loader is removed, for the css img url in this task
+					it is left this way because the checker demands it
+					*/
+					// 'file-loader', 
+					{
+						loader: 'image-webpack-loader',
+					}
+				]},
+		]
+	}
+}
