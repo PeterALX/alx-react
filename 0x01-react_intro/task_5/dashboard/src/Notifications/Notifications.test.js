51cb37f0c1f4fb570171d9b2162fa4d7e75cfe3b
PeterALX <quaypeter25@gmail.com>
2024-11-13 18:04:36 +0300

    done react intro!!🗿🗿💯

--- 0x01-react_intro/task_5/dashboard/src/Notifications/Notifications.test.js
+++ 0x01-react_intro/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -1,22 +1,14 @@
+import {shallow} from 'enzyme'
 import Notifications from "./Notifications";
-import { render, screen } from "@testing-library/react";
 
-/**
- * Note: these tests are temporary. They test specific things in the website that are probably
- * going to change soon. I have added them to have fun with testing, since I opted to use
- * react testing library instead of enzyme, and it seems to no have ways to check that an element with
- * a specific classname is rendered(which is what the testing tasks asked)
- */
-test("testing that <Notifications/> renders without crashing", () => {
-  render(<Notifications />); //render throws an error if there's a problem
-});
-test("verify that Notifications renders three list items", () => {
-  render(<Notifications />);
-  expect(screen.getAllByRole("listitem")).toHaveLength(3);
-});
-test("verify that Notifications renders the text Here is the list of notifications", () => {
-  render(<Notifications />);
-  expect(
-    screen.getByText("Here is the list of notifications")
-  ).toBeInTheDocument();
-});
+test('Notifications renders without crashing', () => {
+	shallow(<Notifications />);
+})
+test('Notifications renders three list items', () => {
+	const wrapper = shallow(<Notifications />);
+	expect(wrapper.find('li')).toHaveLength(3);
+})
+test('Notifications renders the text Here is the list of notifications', () => {
+	const wrapper = shallow(<Notifications />);
+	expect(wrapper.text()).toContain('Here is the list of notifications');
+})
--- 0x01-react_intro/task_5/dashboard/src/utils/utils.test.js
+++ 0x01-react_intro/task_5/dashboard/src/utils/utils.test.js
@@ -2,8 +2,10 @@ import { getFullYear, getFooterCopy, getLatestNotification } from "./utils";
 test("testing getFullYear", () => {
   expect(getFullYear()).toBe(new Date().getFullYear());
 });
-test("testing getFooterCopy", () => {
+test("testing getFooterCopy when passed true", () => {
   expect(getFooterCopy(true)).toBe("Holberton School");
+});
+test("testing getFooterCopy when passed false", () => {
   expect(getFooterCopy(false)).toBe("Holberton School main dashboard");
 });
 test("testing getLatestNotification", () => {
